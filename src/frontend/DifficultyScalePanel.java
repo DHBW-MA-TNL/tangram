a0fccea59189d084d3fa850c34bede83559f25a3
Tristan Lennart Finke <tl.finke@web.de>
2024-05-27 18:50:18 +0200

    adjusted colors

--- src/frontend/DifficultyScalePanel.java
+++ src/frontend/DifficultyScalePanel.java
@@ -4,26 +4,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DifficultyScalePanel extends JPanel {
+    private Color startColor;
+    private Color endColor;
+
+    public DifficultyScalePanel(Color startColor, Color endColor) {
+        this.startColor = startColor;
+        this.endColor = endColor;
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Erzeuge einen Farbverlauf in einem 45-Grad-Winkel
         int width = getWidth();
         int height = getHeight();
 
         // Startpunkt (unten links) und Endpunkt (oben rechts) des Farbverlaufs
-        float[] fractions = {0.0f, 0.5f, 1.0f};
-        Color[] colors = {Color.GREEN, Color.YELLOW, Color.RED};
+        float[] fractions = {0.0f, 1.0f};
+        Color[] colors = {startColor, endColor};
 
         LinearGradientPaint gradient = new LinearGradientPaint(
                 0, 0, width, height, fractions, colors, MultipleGradientPaint.CycleMethod.NO_CYCLE
         );
 
-
         g2d.setPaint(gradient);
-        g2d.fillRoundRect(10, getHeight() / 2 - 20, getWidth() - 20, 40, 20, 20);  // Zeichne ein abgerundetes Rechteck
+        g2d.fillRoundRect(0, 0, width, height, 20, 20);  // Fülle das Button-Rechteck mit dem Farbverlauf
     }
 }
