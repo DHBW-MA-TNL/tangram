2d78ac4238fc3fd26c2854a1b00663886ebf1a12
Luca Krawczyk <luca@calchera.de>
2024-06-05 20:35:53 +0200

    changed

---
+++ src/UiElement.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class UiElement {
+    private Polygon shape;
+    private Color color;
+
+    public UiElement(Polygon shape, Color color) {
+        this.shape = shape;
+        this.color = color;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillPolygon(shape);
+    }
+}
