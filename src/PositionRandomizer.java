7602d311a107ea855e1acb50ba796963e6bbe814
Tristan Lennart Finke <tl.finke@web.de>
2024-06-05 22:19:33 +0200

    Spielerei zum verstehen

--- src/PositionRandomizer.java
+++ src/PositionRandomizer.java
@@ -30,35 +30,56 @@ public class PositionRandomizer {
         current.alignNPointToPoint(0, start);
         int randomNPoint = random.nextInt(current.shape.npoints);
 
-
+        System.out.println("In der Methode");
 
 
         Point next = new Point(current.shape.xpoints[randomNPoint], current.shape.ypoints[randomNPoint]);
 
-        boolean intersects = false;
-        if (!used.isEmpty()) {
-            for (Point vertex : vertices) {
-                for (TangramShape shape : used) {
-                    for (int i = 0; i < 8; i++) {
-                        if (TangramGame.getEdges(shape.shape).stream().anyMatch(edge -> edge.intersectsLine(vertex.x, vertex.y, next.x, next.y))) {
-                            for(int j = 0; j < current.shape.npoints; j++){
-                                if(j!=randomNPoint){
-                                    vertices.add(new Point(current.shape.xpoints[j], current.shape.ypoints[j]));
+        List<Point> newVertices = new ArrayList<>();
+        List<Shape> newUsedList = new ArrayList<>();
+        if (vertices.size() == 0) {
+            for (int j = 0; j < current.shape.npoints; j++) {
+                if (j != randomNPoint) {
+                    vertices.add(new Point(current.shape.xpoints[j], current.shape.ypoints[j]));
+                }
+            }
+            newVertices = vertices;
+        } else {
+
+
+            boolean intersects = false;
+            if (!used.isEmpty()) {
+                System.out.println("In der If");
+                System.out.println("---------------" + vertices.size());
+                for (Point vertex : vertices) {
+                    System.out.println("In der For-1");
+                    for (TangramShape shape : used) {
+                        System.out.println("In der For-2");
+                        for (int i = 0; i < 8; i++) {
+                            System.out.println("In der For-3");
+                            if (TangramGame.getEdges(shape.shape).stream().anyMatch(edge -> edge.intersectsLine(vertex.x, vertex.y, next.x, next.y))) {
+                                System.out.println("----------" + i + " mal gedreht----------------");
+                                for (int j = 0; j < current.shape.npoints; j++) {
+                                    if (j != randomNPoint) {
+                                        newVertices.add(new Point(current.shape.xpoints[j], current.shape.ypoints[j]));
+                                    }
                                 }
+                                System.out.println("Ja");
+                                break;
+                            } else {
+                                current.rotateAroundPoint(start, 45);
+                                System.out.println(i + " mal rotiert");
                             }
-                            break;
-                        } else {
-                            current.rotateAroundPoint(start, 45);
                         }
                     }
+                    System.out.println("aligning to point: " + vertex);
+                    current.alignNPointToPoint(randomNPoint, vertex);
                 }
-                System.out.println("aligning to point: " + vertex);
-                current.alignNPointToPoint(randomNPoint, vertex);
-            }
 
+            }
         }
         used.add(unused.remove(0));
         System.out.println("used size: " + used.size());
-        return shufflePolygons(unused, used, next.x, next.y, vertices);
+        return shufflePolygons(unused, used, next.x, next.y, newVertices);
     }
 }
