02ddbd3592318eaaf2b1fe6c4b1d264a5cdcf1c5
Luca Krawczyk <luca@calchera.de>
2024-06-19 21:18:53 +0200

    renamed class

--- src/frontend/GrayShapes.java
+++ src/frontend/GrayShapes.java
@@ -7,9 +7,9 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LevelShapes{
-    public List<TangramShape> levelShapes= new ArrayList<>();
-    public LevelShapes(){
+public class GrayShapes{
+    public List<TangramShape> grayShapes= new ArrayList<>();
+    public GrayShapes(){
         int multiplier = 2;
         Polygon D1 = multiplyPolygon(new Polygon(new int[]{0, 100, 50}, new int[]{0, 0, 50}, 3), multiplier);
         Polygon D2 = multiplyPolygon(new Polygon(new int[]{100, 50, 100}, new int[]{0, 50, 100}, 3), multiplier);
@@ -19,13 +19,13 @@ public class LevelShapes{
         Polygon Q1 = multiplyPolygon(new Polygon(new int[]{25, 50, 25, 0}, new int[]{25, 50, 75, 50}, 4), multiplier);
         Polygon P1 = multiplyPolygon(new Polygon(new int[]{25, 75, 100, 50}, new int[]{75, 75, 100, 100}, 4), multiplier);
 
-        levelShapes.add(new TangramShape(D1, Color.GRAY));
-        levelShapes.add(new TangramShape(D2, Color.GRAY));
-        levelShapes.add(new TangramShape(D3, Color.GRAY));
-        levelShapes.add(new TangramShape(D4, Color.GRAY));
-        levelShapes.add(new TangramShape(D5, Color.GRAY));
-        levelShapes.add(new TangramShape(Q1, Color.GRAY));
-        levelShapes.add(new TangramShape(P1, Color.GRAY));
+        grayShapes.add(new TangramShape(D1, Color.GRAY));
+        grayShapes.add(new TangramShape(D2, Color.GRAY));
+        grayShapes.add(new TangramShape(D3, Color.GRAY));
+        grayShapes.add(new TangramShape(D4, Color.GRAY));
+        grayShapes.add(new TangramShape(D5, Color.GRAY));
+        grayShapes.add(new TangramShape(Q1, Color.GRAY));
+        grayShapes.add(new TangramShape(P1, Color.GRAY));
     }
     public static Polygon multiplyPolygon(Polygon polygon, int multiplier) {
         int[] xpoints = new int[polygon.npoints];
@@ -39,7 +39,7 @@ public class LevelShapes{
         return new Polygon(xpoints, ypoints, polygon.npoints);
     }
 
-    public List<TangramShape> getlevelShapes(){
-        return levelShapes;
+    public List<TangramShape> getGrayShapes(){
+        return grayShapes;
     }
 }
